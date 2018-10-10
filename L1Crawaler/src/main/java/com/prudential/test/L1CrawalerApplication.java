a5046200b33c078cf206bdaf318d62df382951b1
mrigank-kishore <mrigank.online@gmail.com>
2018-10-10 09:15:02 +0530

    added message for no argument

--- L1Crawaler/src/main/java/com/prudential/test/L1CrawalerApplication.java
+++ L1Crawaler/src/main/java/com/prudential/test/L1CrawalerApplication.java
@@ -19,33 +19,43 @@ public class L1CrawalerApplication {
 
 	public static void main(String[] args) {
 		final Path filePath = Paths.get("./PageDetail.html");
-		System.out.println(args[0] + "==" + args[1]);
-		final int MAX_DEPTH_TO_SEARCH = 2;
-		final Set<String> rootUrl = new HashSet<>();rootUrl.add(args[0].toString());
-		//Set<String> pagesVisited = new HashSet<String>();//for global uniquness
+		if(args.length > 1)	{
+				System.out.println("Program is generating the page details for the website : " + 
+						args[0] + " and the domain is : " + args[1]);
+				System.out.println("Program is generating the output recusively. It will take some time, please wait....");
+				
+				final int MAX_DEPTH_TO_SEARCH = 2;
+				final Set<String> rootUrl = new HashSet<>();rootUrl.add(args[0].toString());
+				//Set<String> pagesVisited = new HashSet<String>();//for global uniquness
 
-		try {
-			if (Files.exists(filePath)) {
-				Files.delete(filePath);
-				Files.createFile(filePath);
-			}else {
-				Files.createFile(filePath);
-			}
+				try {
+					if (Files.exists(filePath)) {
+						Files.delete(filePath);
+						Files.createFile(filePath);
+					}else {
+						Files.createFile(filePath);
+					}
 
-			PageContainer pc = new PageContainer();
-			pc.setUniqueInternalUrl(rootUrl);
+					PageContainer pc = new PageContainer();
+					pc.setUniqueInternalUrl(rootUrl);
 
-			for(int pageDepth = 0; pageDepth<MAX_DEPTH_TO_SEARCH; pageDepth++)	{
-				for(String link : pc.getUniqueInternalUrl()) {
-					pc = Spider.getPage(link, pageDepth, args[1].toString());
-					generateHtml(pc.toString(), filePath);
+					for(int pageDepth = 0; pageDepth<MAX_DEPTH_TO_SEARCH; pageDepth++)	{
+						for(String link : pc.getUniqueInternalUrl()) {
+							pc = Spider.getPage(link, pageDepth, args[1].toString());
+							generateHtml(pc.toString(), filePath);
+						}
+
+					}
+				} catch (IOException e) {
+					System.err.println(e.getMessage());
+					e.printStackTrace();
 				}
-
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			}else	{
+				System.out.println("Please provide website link and domain detial for program to run");
+				System.out.println("Example command is: java -jar L1Crawaler-0.0.1-SNAPSHOT.jar http://web.mit.edu/ mit.edu");
+			
 		}
+		
 		SpringApplication.run(L1CrawalerApplication.class, args);
 	}
 
